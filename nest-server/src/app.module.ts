a812b10e2199fcff1f5372beab11c054192689b1
Konstantin Nipot <konstantin.dnd@gmail.com>
2023-06-02 14:38:47 +0300

    bootstrapping database to nest

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from './messages/messages.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule, MongooseModule.forRoot(
+    'mongodb+srv://konstantindnd:zotja2-nIpvow-fuqgud@cluster0.rnylhwj.mongodb.net/crunchaiy?retryWrites=true&w=majority'
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
